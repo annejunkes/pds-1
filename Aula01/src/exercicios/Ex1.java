64fedaad3594cfcc93e52c7010ac1644b0ae3655
annejunkes <anneburkhardtjunkes@gmail.com>
2024-09-16 20:10:10 -0300

    ex1

--- Aula01/src/exercicios/Ex1.java
+++ Aula01/src/exercicios/Ex1.java
@@ -1,9 +1,33 @@
 package exercicios;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ex1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		
+		int numero = scan.nextInt();
+		
+		for(int i=0; i<10; i++) {
+			
+			System.out.println(numero + " x " + (i+1) + " = " + (numero*(i+1)));
+		}
+		
+		File tabuada = new File("tabuada_" + numero + ".txt");
+		
+		FileWriter fw = new FileWriter(tabuada);
+		
+
+		for(int i=0; i<10; i++) {
+			
+			fw.write(numero + " x " + (i+1) + " = " + (numero*(i+1))+ "\n");
+		}
+		
+		fw.close();
 
 	}
 
